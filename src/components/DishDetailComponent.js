612e33188c8f0eca19a94ccbad8b03a43bc74355
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-07-20 21:00:57 -0400

    Added Redux Action for submitting a comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -28,8 +28,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("current state is: " + JSON.stringify(values));
-        alert("current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
     
     render() {
@@ -105,7 +105,7 @@ class CommentForm extends Component {
     }  
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
 
     const commentList = comments.map((comment) => {
         return (
@@ -126,7 +126,7 @@ function RenderComments({comments}) {
                 <h4>Comments</h4>
                 <div>
                     {commentList}
-                    <CommentForm />
+                    <CommentForm dishId={dishId} addComment={addComment}/>
                 </div>
             </div>
         );
@@ -171,7 +171,10 @@ const DishDetail = (props) => {
                 </div>
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments} 
+                        addComment={props.addComment}
+                        dishId={props.dish.id}
+                    />
                 </div>
             </div>
         );
