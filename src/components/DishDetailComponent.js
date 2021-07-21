0dcb93b622bd0c5045865f9477a4fe14eda7bc52
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-07-21 12:12:21 -0400

    added redux thunk and redux logger to project, along with a loading circle

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Col, Row, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 
@@ -155,6 +156,25 @@ function RenderDish({dish}) {
 
 
 const DishDetail = (props) => {
+
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errmess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errmess}</h4>
+                </div>
+            </div>
+        );
+    }
 
     if (props.dish != null) {
         return (
